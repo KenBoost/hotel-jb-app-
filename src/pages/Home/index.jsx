8f380aed07b27e3e8d6add5d3f0377a1eb52b4c3
Keneth Miranda <kennethmiranda56@gmail.com>
2023-10-24 14:20:19 -0600

    preparing for bootstrap

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>
-      <h1>Bienvenido a la página de inicio</h1>
-      <p>Esta es la página de inicio de nuestra aplicación. Puedes agregar contenido y elementos adicionales aquí.</p>
-      <p>¡Comienza a construir tu aplicación con React!</p>
+    <div className="contenedorprincipal">
+      <div className="cuadro1">
+        <h1>Bienvenido a la página de inicio</h1>
+        <p>
+          Esta es la página de inicio de nuestra aplicación. Puedes agregar
+          contenido y elementos adicionales aquí.
+        </p>
+        <p>¡Comienza a construir tu aplicación con React!</p>
+      </div>
+      <div className="cuadro2"></div>
+      <div className="cuadro3">
+        
+      </div>
     </div>
   );
 }
