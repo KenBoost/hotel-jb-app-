d5bc00cff3fdbe425c0e3314a653df96af3e2e7c
Keneth Miranda <kennethmiranda56@gmail.com>
2023-10-27 16:56:02 -0600

    Details

--- src/components/Importedimages/index.jsx
+++ src/components/Importedimages/index.jsx
@@ -1,6 +1,6 @@
 import Carrusel10 from '../../assets/images/visuals/carrusel40.png'
 import Carrusel20 from '../../assets/images/visuals/carrusel2.jpeg'
-import Carrusel30 from '../../assets/images/visuals/carrusel3.jpeg'
+import Carrusel30 from '../../assets/images/visuals/carrusel4.jpeg'
 
 
 export { Carrusel10, Carrusel20, Carrusel30};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,12 +9,18 @@ function Home() {
       <Carrusel></Carrusel>
       </div>
       <div className="cuadro2">
-        <h1>Bienvenido a la página de inicio</h1>
+        <h1>A Lodge full of life!</h1>
         <p>
-          Esta es la página de inicio de nuestra aplicación. Puedes agregar
-          contenido y elementos adicionales aquí.
+        If you're seeking a unique experience on the Caribbean coast of Costa Rica, our lodge in 
+        Puerto Viejo de Limón is your ideal destination. 
+        Situated amidst the lush rainforest, our modern retreat offers 
+        you the opportunity to be immersed in nature at its finest. 
+        Here, comfort and adventure seamlessly blend. Discover the natural 
+        beauty that envelops our lodge while enjoying modern facilities and 
+        basking in the tranquility of the jungle. Immerse yourself in the 
+        serenity and wonder of nature in its purest form.
         </p>
-        <p>¡Comienza a construir tu aplicación con React!</p>
+  
       </div>
       <div className="cuadro3">
     
