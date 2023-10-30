e258d0b4fdf9270b0231ad6245d595b022bf1484
Keneth Miranda <kennethmiranda56@gmail.com>
2023-10-30 13:49:07 -0600

    Connecting the Contact Form

--- backend/CRUD.py
+++ backend/CRUD.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from pymongo import MongoClient, errors
 from bson import ObjectId
 from flask_cors import CORS
+import smtplib
 
 import bcrypt #Modulo para encriptar y desencriptar
 
@@ -11,6 +12,37 @@ app = Flask(__name__)
 # Configuracion de la base de datos en MongoDB
 client = MongoClient("mongodb://127.0.0.1:27017/")
 db = client['hotel-junglebreeze']
+
+
+
+#------------ENVIO DE CORREOS-------------------------------------------
+#-----------------------------------------------------------------------
+@app.route('/', methods=['POST'])
+def contact_form():
+    if request.method == 'POST':
+        nombre = request.form.get('nombre')
+        correo = request.form.get('correo')
+        mensaje = request.form.get('mensaje')
+
+        # Configura el envío de correo electrónico
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login('kennethmiranda56@gmail.com', 'kennethmcr16')
+
+        subject = 'Nuevo mensaje de Jungle Breeze'
+        body = f'Nombre: {nombre}\nCorreo: {correo}\nMensaje: {mensaje}'
+        msg = f'Subject: {subject}\n\n{body}'
+
+        server.sendmail('tucorreogmail@gmail.com', 'tudirecciondestino@gmail.com', msg)
+
+        server.quit()
+
+        # Devuelve una respuesta JSON de éxito
+        response = {'message': 'El mensaje se envió con éxito'}
+        return jsonify(response), 200
+
+
+
 
 #-----------------------------------------------------------------------
 #CROSS ORIGINS-----------------------------------------------------------
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,55 +1,94 @@
-import React, {  useState } from 'react'; // Importa useState
-import './index.scss'
-import Map from '../../components/Map'
+import React, { useState } from "react"; // Importa useState
+import "./index.scss";
+import Map from "../../components/Map";
+import Axios from "axios";
+import swal from 'sweetalert2'; // Importa SweetAlert
+
 
 const Contact = () => {
   const [formData, setFormData] = useState({
-    nombre: '',
-    correo: '',
-    mensaje: ''
+    nombre: "",
+    correo: "",
+    mensaje: "",
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Aquí puedes manejar la lógica para enviar los datos del formulario
-    console.log(formData); // Ejemplo: muestra los datos en la consola
-  }
+    Axios.post("http://localhost:5000", formData)
+      .then((response) => {
+        if (response.status === 200) {
+          swal.fire('Mensaje enviado', 'El mensaje se envió con éxito', 'success');
+          // Puedes redirigir al usuario a una página de confirmación o realizar otras acciones apropiadas aquí.
+        } else {
+          swal.fire('Error', 'Ocurrió un error al enviar el mensaje', 'error');
+        }
+      })
+      .catch((error) => {
+        console.error("Error en la solicitud POST", error);
+        swal.fire('Error', 'Ocurrió un error al enviar el mensaje', 'error');
+      });
+  };
 
   return (
-    <div className='contenedorcontacto'>
-      <div className='cont1'>
+    <div className="contenedorcontacto">
+      <div className="cont1">
         <h3>Contact us for any information</h3>
-        <form onSubmit={handleSubmit} className='formulario'>
+        <form onSubmit={handleSubmit} className="formulario">
           <div className="form-group">
             <label htmlFor="nombre">Name</label>
-            <input type="text" id="nombre" name="nombre" value={formData.nombre} onChange={handleChange} required placeholder="Enter your full name.." />
+            <input
+              type="text"
+              id="nombre"
+              name="nombre"
+              value={formData.nombre}
+              onChange={handleChange}
+              required
+              placeholder="Enter your full name.."
+            />
           </div>
           <div className="form-group">
             <label htmlFor="correo">Email</label>
-            <input type="email" id="correo" name="correo" value={formData.correo} onChange={handleChange} required placeholder="Enter your email address.." />
+            <input
+              type="email"
+              id="correo"
+              name="correo"
+              value={formData.correo}
+              onChange={handleChange}
+              required
+              placeholder="Enter your email address.."
+            />
           </div>
           <div className="form-group">
             <label htmlFor="mensaje">What's your question?</label>
-            <textarea id="mensaje" name="mensaje" value={formData.mensaje} onChange={handleChange} rows="4" required placeholder="Type your message here.." style={{ resize: 'none' }}></textarea>
+            <textarea
+              id="mensaje"
+              name="mensaje"
+              value={formData.mensaje}
+              onChange={handleChange}
+              rows="4"
+              required
+              placeholder="Type your message here.."
+              style={{ resize: "none" }}
+            ></textarea>
           </div>
           <div className="form-group">
             <button type="submit">Send</button>
           </div>
         </form>
       </div>
-      <div className='cont2'>
+      <div className="cont2">
         <Map></Map>
       </div>
     </div>
   );
-}
+};
 
 export default Contact;
