55525b309d74a78bdf12f7df706930cbbadac48d
Keneth Miranda <kennethmiranda56@gmail.com>
2023-10-30 13:28:58 -0600

    rooms header done

--- src/components/Importedimages/index.jsx
+++ src/components/Importedimages/index.jsx
@@ -2,6 +2,8 @@ import Carrusel10 from '../../assets/images/visuals/carrusel40.png'
 import Carrusel20 from '../../assets/images/visuals/carrusel2.jpeg'
 import Carrusel30 from '../../assets/images/visuals/carrusel4.jpeg'
 
+import Habitacion1 from '../../assets/images/habitaciones/habitaciones.jpg'
+import Habitacion2 from '../../assets/images/habitaciones/habitaciones2.jpeg'
 
-export { Carrusel10, Carrusel20, Carrusel30};
+export { Carrusel10, Carrusel20, Carrusel30, Habitacion1, Habitacion2};
 
--- src/pages/Rooms/index.jsx
+++ src/pages/Rooms/index.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import './index.scss'
+import { Habitacion1, Habitacion2} from "../../components/Importedimages";
 
 
-const Room = () =>{
-    return(
-       <h2>Hi</h2>
-    )
-}
-export default Room
+const Room = () => {
+  return (
+    <div>
+      <div className="partearriba">
+        <h2>BUNGALOWS</h2>
+      </div>
+      <div className='partemedia'>
+        <div className='room1'></div>
+        <div className='room2'></div>
+        <div className='room3'></div>
+      </div>
+    </div>
+  );
+};
+export default Room;
